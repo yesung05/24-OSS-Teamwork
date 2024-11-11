a116095abb699c2e1dbe951215b138e294e6cb55
yesung05 <jyslove05@gmail.com>
2024-11-11 15:55:21 +0900

    OX 문제 수정, 개수 초과시 출력 변경

--- QType/OX.py
+++ QType/OX.py
@@ -1,7 +1,7 @@
 def OX(num,Question): # 단답형 문제 함수
     print(f"{num}. {Question[3]}")
     UserAnswer = input("정답(O, X): ")[0]
-    UserAnswer = UserAnswer.lower()
+    UserAnswer = UserAnswer.upper()
 
     if(Question[2]==UserAnswer):
         print("정답입니다.")
--- main.py
+++ main.py
@@ -18,11 +18,16 @@ datas = readcsv.readcsv(file_path)
 # 데이터 섞기
 random.shuffle(datas)
 
+sel = True
 # 문제 풀기
-count = int(input("몇 문제를 풀 것인지 정해주세요."))
-if(count > len(datas)): # 만약 입력한 문제 수가 문제집 문제 수를 초과하면 (수정 예정)
-    print(f"문제집에 등록된 문제의 수가 {len(datas)}개입니다.")
-    count = len(datas)
+while(sel):
+    print(f"선택된 문제집에 등록된 문제의 수가 {len(datas)}개입니다.")
+    count = int(input("몇 문제를 풀 것인지 정해주세요."))
+    if(count > len(datas)): # 만약 입력한 문제 수가 문제집 문제 수를 초과하면 (수정 예정)
+        print(f"{len(datas)}개의 이하의 값을 입력해주세요.")
+        continue
+
+    sel = False
 print()
 print('------------------------------------------')
 print()
