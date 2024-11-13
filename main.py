af5b6f8c38be76d6f65a266d53436238ec7e2550
yesung05 <jyslove05@gmail.com>
2024-11-13 14:18:05 +0900

    문제풀이 부분 분리

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import QType as pqt  # Print Question Type
 import ReturnQuestionType as rqt  # Return Question Type
 import readfile
 import readcsv
+import runq
 """
 **필드명**
 
@@ -22,7 +23,13 @@ sel = True
 # 문제 풀기
 while(sel):
     print(f"선택된 문제집에 등록된 문제의 수가 {len(datas)}개입니다.")
-    count = int(input("몇 문제를 풀 것인지 정해주세요."))
+    count = input("몇 문제를 풀 것인지 정해주세요. (다시 선택하시려면 'q'를 입력해주세요.): ")
+    if(count == 'q'): 
+        file_path = readfile.readfile()
+        datas = readcsv.readcsv(file_path)
+        continue
+    else:
+        count = int(count)
     if(count > len(datas)): # 만약 입력한 문제 수가 문제집 문제 수를 초과하면 (수정 예정)
         print(f"{len(datas)}개의 이하의 값을 입력해주세요.")
         continue
@@ -31,16 +38,16 @@ while(sel):
 print()
 print('------------------------------------------')
 print()
+runq.runqq(count,datas)
+# for i in range(count):  # 원하는 문제 횟수로 반복
 
-for i in range(count):  # 원하는 문제 횟수로 반복
+#     Qtype = rqt.getQuestionType(datas[i])
+#     if Qtype == "객관식":
+#         pqt.MultiChoice(i+1, datas[i])
+#     elif Qtype == "단답형":
+#         pqt.ShortAnswer(i+1, datas[i])
+#     elif Qtype == "O, X":
+#         pqt.OX(i+1, datas[i])
+#     print('------------------------------------------')
 
-    Qtype = rqt.getQuestionType(datas[i])
-    if Qtype == "객관식":
-        pqt.MultiChoice(i+1, datas[i])
-    elif Qtype == "단답형":
-        pqt.ShortAnswer(i+1, datas[i])
-    elif Qtype == "O, X":
-        pqt.OX(i+1, datas[i])
-    print('------------------------------------------')
-
-input()
+# input()
---
+++ runq.py
@@ -0,0 +1,13 @@
+import QType as pqt
+import ReturnQuestionType as rqt
+def runqq(count, datas):
+    for i in range(count):  # 원하는 문제 횟수로 반복
+
+        Qtype = rqt.getQuestionType(datas[i])
+        if Qtype == "객관식":
+            pqt.MultiChoice(i+1, datas[i])
+        elif Qtype == "단답형":
+            pqt.ShortAnswer(i+1, datas[i])
+        elif Qtype == "O, X":
+            pqt.OX(i+1, datas[i])
+        print('------------------------------------------')
